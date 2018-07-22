434dc1d90bba0f60a761c27db09b45ee4d59d4d6
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-07-22 20:49:59 +0200

    Added theming support for the TabItem.

--- Celestial.UIToolkit/Theming/TabControlProperties.cs
+++ Celestial.UIToolkit/Theming/TabControlProperties.cs
@@ -18,6 +18,12 @@ namespace Celestial.UIToolkit.Theming
             "SelectedMarkHeight", typeof(double), typeof(TabControlProperties), new PropertyMetadata(1d));
 
         /// <summary>
+        /// Identifies the SelectedMarkMargin attached dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SelectedMarkMarginProperty = DependencyProperty.RegisterAttached(
+            "SelectedMarkMargin", typeof(Thickness), typeof(TabControlProperties), new PropertyMetadata(new Thickness()));
+
+        /// <summary>
         /// Gets the value of the <see cref="SelectedMarkHeightProperty"/> attached dependency property
         /// for a given <see cref="DependencyObject"/>.
         /// </summary>
@@ -52,6 +58,41 @@ namespace Celestial.UIToolkit.Theming
             obj.SetValue(SelectedMarkHeightProperty, value);
         }
         
+        /// <summary>
+        /// Gets the value of the <see cref="SelectedMarkMarginProperty"/> attached dependency property
+        /// for a given <see cref="DependencyObject"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="DependencyObject"/> for which the local value of the
+        /// <see cref="SelectedMarkMarginProperty"/> attached dependency property
+        /// should be retrieved.
+        /// </param>
+        /// <returns>
+        /// The local value of the <see cref="SelectedMarkMarginProperty"/> attached dependency property,
+        /// which is of type <see cref="Thickness"/>.
+        /// </returns>
+        public static Thickness GetSelectedMarkMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(SelectedMarkMarginProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="SelectedMarkMarginProperty"/> attached dependency property
+        /// for a given <see cref="DependencyObject"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="DependencyObject"/> for which the local value of the
+        /// <see cref="SelectedMarkMarginProperty"/> attached dependency property
+        /// should be set.
+        /// </param>
+        /// <param name="value">
+        /// The new value for the dependency property.
+        /// </param>
+        public static void SetSelectedMarkMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(SelectedMarkMarginProperty, value);
+        }
+
     }
 
 }
