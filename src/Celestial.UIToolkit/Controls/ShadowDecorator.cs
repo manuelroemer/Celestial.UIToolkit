09f8c120244f7d9e8b6460b402c03e1ec8ce1ef5
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-07 01:39:37 +0200

    Added a functional ShadowDecorator

---
+++ src/Celestial.UIToolkit/Controls/ShadowDecorator.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace Celestial.UIToolkit.Controls
+{
+
+    /// <summary>
+    /// Defines the different types of shadows that are available.
+    /// </summary>
+    public enum ShadowType
+    {
+
+        /// <summary>
+        /// An ambient shadow is caused by ambient light.
+        /// Shadows caused by this type of light appear at
+        /// all angles of the control.
+        /// </summary>
+        Ambient,
+
+        /// <summary>
+        /// A directional shadow is caused by a light that
+        /// comes from a specific direction.
+        /// </summary>
+        Directional
+
+    }
+
+    /// <summary>
+    /// A decorator which applies a drop shadow effect to the underlying
+    /// child element.
+    /// In essence, this is a wrapper for the <see cref="DropShadowEffect"/>
+    /// which simplifies the interacting with its properties.
+    /// In addition though, this decorator is based on the elevation system
+    /// used in the Material Design Language.
+    /// </summary>
+    public class ShadowDecorator : Decorator
+    {
+
+        private DropShadowEffect _dropShadowEffect;
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowColor"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowColorProperty = DependencyProperty.Register(
+            nameof(ShadowColor), 
+            typeof(Color), 
+            typeof(ShadowDecorator), 
+            new FrameworkPropertyMetadata(
+                Colors.Black,
+                FrameworkPropertyMetadataOptions.AffectsRender, 
+                ShadowProperty_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowOpacity"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowOpacityProperty = DependencyProperty.Register(
+            nameof(ShadowOpacity), 
+            typeof(double), 
+            typeof(ShadowDecorator), 
+            new FrameworkPropertyMetadata(
+                1d, 
+                FrameworkPropertyMetadataOptions.AffectsRender, 
+                ShadowProperty_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowType"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowTypeProperty = DependencyProperty.Register(
+            nameof(ShadowType), 
+            typeof(ShadowType), 
+            typeof(ShadowDecorator), 
+            new FrameworkPropertyMetadata(
+                ShadowType.Ambient, 
+                FrameworkPropertyMetadataOptions.AffectsRender, 
+                ShadowProperty_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="Elevation"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ElevationProperty = DependencyProperty.Register(
+            nameof(Elevation), 
+            typeof(double), 
+            typeof(ShadowDecorator), 
+            new FrameworkPropertyMetadata(
+                1d, 
+                FrameworkPropertyMetadataOptions.AffectsRender, 
+                ShadowProperty_Changed),
+            (value) => (double)value >= 0);
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowDirection"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowDirectionProperty = DependencyProperty.Register(
+            nameof(ShadowDirection), 
+            typeof(Dock), 
+            typeof(ShadowDecorator), 
+            new FrameworkPropertyMetadata(
+                Dock.Bottom, 
+                FrameworkPropertyMetadataOptions.AffectsRender, 
+                ShadowProperty_Changed));
+
+        /// <summary>
+        /// Gets or sets the shadow's colors.
+        /// </summary>
+        public Color ShadowColor
+        {
+            get { return (Color)GetValue(ShadowColorProperty); }
+            set { SetValue(ShadowColorProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets the shadow's opacity as a value between 0 and 1.
+        /// </summary>
+        public double ShadowOpacity
+        {
+            get { return (double)GetValue(ShadowOpacityProperty); }
+            set { SetValue(ShadowOpacityProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets the type of shadow which is currently displayed by this decorator.
+        /// </summary>
+        public ShadowType ShadowType
+        {
+            get { return (ShadowType)GetValue(ShadowTypeProperty); }
+            set { SetValue(ShadowTypeProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the elevation of the element which is being decorated with
+        /// this <see cref="ShadowDecorator"/>.
+        /// The elevation defines the strength of the shadow.
+        /// The higher the elevation of the element, the stronger the shadow.
+        /// </summary>
+        public double Elevation
+        {
+            get { return (double)GetValue(ElevationProperty); }
+            set { SetValue(ElevationProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets the direction of the shadow, if <see cref="ShadowType"/>
+        /// is set to <see cref="ShadowType.Directional"/>.
+        /// </summary>
+        public Dock ShadowDirection
+        {
+            get { return (Dock)GetValue(ShadowDirectionProperty); }
+            set { SetValue(ShadowDirectionProperty, value); }
+        }
+        
+        public ShadowDecorator()
+        {
+            _dropShadowEffect = new DropShadowEffect();
+        }
+
+        private static void ShadowProperty_Changed(DependencyObject d, DependencyPropertyChangedEventArgs baseValue)
+        {
+            var self = (ShadowDecorator)d;
+            self.UpdateDecoratorEffect();
+        }
+        
+        private void UpdateDecoratorEffect()
+        {
+            if (this.Elevation == 0)
+            {
+                this.Effect = null;
+            }
+            else
+            {
+                this.UpdateDropShadow();
+                this.Effect = _dropShadowEffect;
+            }
+        }
+        
+        private void UpdateDropShadow()
+        {
+            var valueCalculator = this.CreateShadowEffectValueCalculator();
+            _dropShadowEffect.ShadowDepth = valueCalculator.CalculateShadowDepth();
+            _dropShadowEffect.Direction = valueCalculator.CalculateDirection();
+            _dropShadowEffect.BlurRadius = valueCalculator.CalculateBlurRadius();
+            _dropShadowEffect.Color = this.ShadowColor;
+            _dropShadowEffect.Opacity = this.ShadowOpacity;
+        }
+
+        private DropShadowValueCalculator CreateShadowEffectValueCalculator()
+        {
+            if (this.ShadowType == ShadowType.Ambient)
+                return new AmbientDropShadowValueCalculator(this.Elevation, this.ShadowDirection);
+            else if (this.ShadowType == ShadowType.Directional)
+                return new DirectionalDropShadowValueCalculator(this.Elevation, this.ShadowDirection);
+            else
+                throw new NotImplementedException("Unknown ShadowType.");
+        }
+        
+        private abstract class DropShadowValueCalculator
+        {
+            protected double Elevation { get; }
+            protected Dock ShadowDirection { get; }
+
+            public DropShadowValueCalculator(double elevation, Dock shadowDirection)
+            {
+                this.Elevation = elevation;
+                this.ShadowDirection = shadowDirection;
+            }
+
+            public abstract double CalculateShadowDepth();
+
+            public abstract double CalculateDirection();
+
+            public virtual double CalculateBlurRadius()
+            {
+                // This value is chosen through observation and testing.
+                // It looks the most real, when comparing different elevation levels.
+                const int elevationLevelMultiplier = 5;
+                return this.Elevation * elevationLevelMultiplier;
+            }
+        }
+
+        private sealed class AmbientDropShadowValueCalculator : DropShadowValueCalculator
+        {
+            public AmbientDropShadowValueCalculator(double elevation, Dock shadowDirection)
+                : base(elevation, shadowDirection) { }
+
+            // Ambient shadows point in all directions.
+            // For a DropShadowEffect, that is the case if
+            // ShadowDepth and Direction are both 0.
+            public override double CalculateShadowDepth()
+            {
+                return 0d;
+            }
+
+            public override double CalculateDirection()
+            {
+                return 0d;
+            }
+            
+        }
+        
+        private sealed class DirectionalDropShadowValueCalculator : DropShadowValueCalculator
+        {
+            public DirectionalDropShadowValueCalculator(double elevation, Dock shadowDirection)
+                : base(elevation, shadowDirection) { }
+
+            public override double CalculateShadowDepth()
+            {
+                // ShadowDepth <-> Elevation can be treated on a 1-1 basis.
+                return this.Elevation;
+            }
+
+            public override double CalculateDirection()
+            {
+                // The direction angle is mapped counter-clockwise,
+                // starting from the right.
+                switch (this.ShadowDirection)
+                {
+                    case Dock.Right:
+                        return 0;
+                    case Dock.Top:
+                        return 90;
+                    case Dock.Left:
+                        return 180;
+                    case Dock.Bottom:
+                        return 270;
+                    default:
+                        throw new NotImplementedException(
+                            "Unknown Dock enumeration value. " +
+                            "This really shouldn't ever happen...");
+                }
+            }
+            
+        }
+
+    }
+
+}
