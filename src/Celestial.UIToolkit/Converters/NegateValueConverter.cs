5300ff2177765f3346b2fd683f07b71fd56f98f9
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-07 21:11:30 +0200

    Created a functional Card Template.

---
+++ src/Celestial.UIToolkit/Converters/NegateValueConverter.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Celestial.UIToolkit.Converters
+{
+
+    /// <summary>
+    /// An implementation of the <see cref="IValueConverter"/> interface
+    /// which negates the input variable, if possible.
+    /// This works on numbers and a few WPF types like <see cref="Thickness"/>.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(object))]
+    public class NegateValueConverter : IValueConverter
+    {
+
+        /// <summary>
+        /// Gets a default instance of the <see cref="NegateValueConverter"/>.
+        /// </summary>
+        public static NegateValueConverter Default { get; } = new NegateValueConverter();
+
+        /// <summary>
+        /// Negates the specified value, if the type is supported.
+        /// If not, a <see cref="NotSupportedException"/> is thrown.
+        /// </summary>
+        /// <param name="value">The value to be converted.</param>
+        /// <param name="targetType">Not used.</param>
+        /// <param name="parameter">Not used.</param>
+        /// <param name="culture">Not used.</param>
+        /// <returns>The negated value.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Type valueType = value.GetType();
+            
+            if (typeof(IConvertible).IsAssignableFrom(valueType))
+            {
+                return this.NegateConvertible((IConvertible)value);
+            }
+            else if (valueType == typeof(Thickness))
+            {
+                return this.NegateThickness((Thickness)value);
+            }
+            else if (valueType == typeof(CornerRadius))
+            {
+                return this.NegateCornerRadius((CornerRadius)value);
+            }
+            else if (valueType == typeof(Size))
+            {
+                return this.NegateSize((Size)value);
+            }
+            else if (valueType == typeof(Point))
+            {
+                return this.NegatePoint((Point)value);
+            }
+            else
+            {
+                throw new NotSupportedException(
+                    $"The type '{valueType.FullName}' is not supported by this converter.");
+            }
+        }
+
+        /// <summary>
+        /// Negates the specified value, if the type is supported.
+        /// If not, a <see cref="NotSupportedException"/> is thrown.
+        /// </summary>
+        /// <param name="value">The value to be converted.</param>
+        /// <param name="targetType">Not used.</param>
+        /// <param name="parameter">Not used.</param>
+        /// <param name="culture">Not used.</param>
+        /// <returns>The negated value.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.Convert(value, targetType, parameter, culture);
+        }
+
+        private IConvertible NegateConvertible(IConvertible convertible)
+        {
+            decimal d = System.Convert.ToDecimal(convertible);
+            decimal result = d * -1;
+            return (IConvertible)System.Convert.ChangeType(d, convertible.GetType());
+        }
+
+        private Thickness NegateThickness(Thickness thickness)
+        {
+            return new Thickness(
+                thickness.Left * -1,
+                thickness.Top * -1,
+                thickness.Right * -1,
+                thickness.Bottom * -1);
+        }
+
+        private CornerRadius NegateCornerRadius(CornerRadius cornerRadius)
+        {
+            return new CornerRadius(
+                cornerRadius.TopLeft * -1,
+                cornerRadius.TopRight * -1,
+                cornerRadius.BottomRight * -1,
+                cornerRadius.BottomLeft * -1);
+        }
+
+        private Size NegateSize(Size size)
+        {
+            return new Size(
+                size.Width * -1,
+                size.Height * -1);
+        }
+
+        private Point NegatePoint(Point point)
+        {
+            return new Point(
+                point.X * -1,
+                point.Y * -1);
+        }
+        
+    }
+
+}
---
+++ src/Celestial.UIToolkit/Converters/ThicknessSidePickerConverter.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Celestial.UIToolkit.Converters
+{
+
+    /// <summary>
+    /// An implementation of the <see cref="IValueConverter"/> interface
+    /// which expects a <see cref="Thickness"/>, sets certain edge
+    /// properties to a desired value and then returns the result.
+    /// </summary>
+    [ValueConversion(typeof(Thickness), typeof(Thickness))]
+    public class ThicknessSidePickerConverter : ValueConverter<Thickness, Thickness>
+    {
+
+        /// <summary>
+        /// Gets or sets a value to which the <see cref="Thickness.Left"/>
+        /// value will be set.
+        /// If this is <c>null</c>, the original value is retained.
+        /// </summary>
+        public double? Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value to which the <see cref="Thickness.Top"/>
+        /// value will be set.
+        /// If this is <c>null</c>, the original value is retained.
+        /// </summary>
+        public double? Top { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value to which the <see cref="Thickness.Right"/>
+        /// value will be set.
+        /// If this is <c>null</c>, the original value is retained.
+        /// </summary>
+        public double? Right { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value to which the <see cref="Thickness.Bottom"/>
+        /// value will be set.
+        /// If this is <c>null</c>, the original value is retained.
+        /// </summary>
+        public double? Bottom { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThicknessSidePickerConverter"/>.
+        /// </summary>
+        public ThicknessSidePickerConverter() { }
+
+        /// <summary>
+        /// Changes the values of the input thickness to the values set in this
+        /// converter's properties.
+        /// </summary>
+        /// <param name="value">The thickness whose values will be altered.</param>
+        /// <param name="parameter">Not used.</param>
+        /// <param name="culture">Not used.</param>
+        /// <returns>The altered <see cref="Thickness"/>.</returns>
+        public override Thickness Convert(Thickness value, object parameter, CultureInfo culture)
+        {
+            return new Thickness(
+                this.Left ?? value.Left,
+                this.Top ?? value.Top,
+                this.Right ?? value.Right,
+                this.Bottom ?? value.Bottom);
+        }
+
+    }
+
+}
