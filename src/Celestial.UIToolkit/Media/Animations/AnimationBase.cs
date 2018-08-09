36ec7e92e8ffb71358c221ff26c22ff4cee8a0ec
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-09 20:39:54 +0200

    BrushAnimation base.

---
+++ src/Celestial.UIToolkit/Media/Animations/AnimationBase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Media.Animation;
+
+namespace Celestial.UIToolkit.Media.Animations
+{
+
+    /// <summary>
+    ///     Defines a base class for a custom <see cref="AnimationTimeline"/>,
+    ///     following the pattern in the .NET Framework (see remarks).
+    ///     
+    ///     When using this class as a base for your custom animation,
+    ///     create an abstract <c>TAnimationBase</c> class, deriving from this class.
+    ///     
+    ///     In addition, create a <c>TAnimation</c> class deriving from the TAnimationBase,
+    ///     which then provides the actual logic, while also overriding the leftover types.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type which is being animated by the animation.
+    /// </typeparam>
+    /// <remarks>
+    ///     For more information about the intended design of animation classes,
+    ///     see the following information:
+    ///     https://docs.microsoft.com/de-de/dotnet/framework/wpf/graphics-multimedia/custom-animations-overview#derive-from-animationtimeline
+    /// </remarks>
+    public abstract class AnimationBase<T> : AnimationTimeline
+    {
+
+        /// <summary>
+        ///     Gets the target type of the animation..
+        /// </summary>
+        public override Type TargetPropertyType => typeof(T);
+
+        /// <summary>
+        ///     Returns the current value of the animation.
+        /// </summary>
+        /// <param name="defaultOriginValue">
+        ///     The origin value provided to the animation if the animation does not have its
+        ///     own start value. If this animation is the first in a composition chain it will
+        ///     be the base value of the property being animated; otherwise it will be the value
+        ///     returned by the previous animation in the chain.
+        /// </param>
+        /// <param name="defaultDestinationValue">
+        ///     The destination value provided to the animation if the animation does not have
+        ///     its own destination value.
+        /// </param>
+        /// <param name="animationClock">
+        ///     The <see cref="AnimationClock"/> which can generate the <see cref="Clock.CurrentTime"/>
+        ///     or <see cref="Clock.CurrentProgress"/> value to be used by the
+        ///     animation to generate its output value.
+        /// </param>
+        /// <returns>The value this animation believes should be the current value for the property.</returns>
+        public override object GetCurrentValue(
+            object defaultOriginValue,
+            object defaultDestinationValue,
+            AnimationClock animationClock)
+        {
+            if (!(defaultOriginValue is T))
+                this.ThrowForInvalidAnimationValue(nameof(defaultOriginValue));
+            if (!(defaultDestinationValue is T))
+                this.ThrowForInvalidAnimationValue(nameof(defaultDestinationValue));
+
+            return this.GetCurrentValueCore(
+                (T)defaultOriginValue, (T)defaultDestinationValue, animationClock);
+        }
+
+        /// <summary>
+        ///     Calculates a value that represents the current value of the property being animated, 
+        ///     as determined by the host animation.
+        /// </summary>
+        /// <param name="defaultOriginValue">
+        ///      The suggested origin value, used if the animation 
+        ///      does not have its own explicitly set start value.
+        /// </param>
+        /// <param name="defaultDestinationValue">
+        ///     The suggested destination value, used if the animation 
+        ///     does not have its own explicitly set end value.
+        /// </param>
+        /// <param name="animationClock">
+        ///     The <see cref="AnimationClock"/> which can generate the <see cref="Clock.CurrentTime"/>
+        ///     or <see cref="Clock.CurrentProgress"/> value to be used by the
+        ///     animation to generate its output value.
+        /// </param>
+        /// <returns>The value this animation believes should be the current value for the property.</returns>
+        protected abstract T GetCurrentValueCore(
+            T defaultOriginValue,
+            T defaultDestinationValue,
+            AnimationClock animationClock);
+        
+        private void ThrowForInvalidAnimationValue(string paramName)
+        {
+            throw new ArgumentException(
+                $"The animation does not support the type of the provided {paramName}. " +
+                $"It expected a parameter of type {typeof(T).FullName}.",
+                paramName);
+        }
+
+    }
+    
+}
---
+++ src/Celestial.UIToolkit/Media/Animations/BrushAnimation.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Celestial.UIToolkit.Media.Animations
+{
+
+    /// <summary>
+    /// An abstract base class which, when implemented, animates
+    /// a <see cref="Brush"/>.
+    /// </summary>
+    public abstract class BrushAnimationBase : AnimationBase<Brush> { }
+
+    /// <summary>
+    ///     An animation which is able to animate <see cref="SolidColorBrush"/>,
+    ///     <see cref="LinearGradientBrush"/> and <see cref="RadialGradientBrush"/>
+    ///     objects.
+    /// </summary>
+    /// <remarks>
+    ///     Even though the animation supports a set of different brushes,
+    ///     it will typically not be able to do "inter-brush" animations.
+    ///     One animation can only animate a single type of brush.
+    ///     
+    ///     In addition, when animating gradient brushes, the animation can
+    ///     only deal with brush values which have a fixed number of
+    ///     gradient stops.
+    ///     Deviations from this number will cause the animation to fail.
+    /// </remarks>
+    public class BrushAnimation : BrushAnimationBase
+    {
+        
+        /// <summary>
+        /// Identifies the <see cref="From"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty FromProperty = DependencyProperty.Register(
+            nameof(From), typeof(Brush), typeof(BrushAnimation), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="To"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ToProperty = DependencyProperty.Register(
+            nameof(To), typeof(Brush), typeof(BrushAnimation), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets a <see cref="Brush"/> which serves as the animation's
+        /// starting value.
+        /// </summary>
+        public Brush From
+        {
+            get { return (Brush)GetValue(FromProperty); }
+            set { SetValue(FromProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets a <see cref="Brush"/> which serves as the animation's ending value.
+        /// </summary>
+        public Brush To
+        {
+            get { return (Brush)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+        
+        /// <summary>
+        /// Returns a new instance of the <see cref="BrushAnimation"/> class.
+        /// </summary>
+        /// <returns>A new <see cref="BrushAnimation"/> instance.</returns>
+        protected override Freezable CreateInstanceCore() => new BrushAnimation();
+
+        /// <summary>
+        ///     Calculates a value that represents the current value of the property being animated, 
+        ///     as determined by the host animation.
+        /// </summary>
+        /// <param name="defaultOriginValue">
+        ///      The suggested origin value, used if the animation 
+        ///      does not have its own explicitly set start value.
+        /// </param>
+        /// <param name="defaultDestinationValue">
+        ///     The suggested destination value, used if the animation 
+        ///     does not have its own explicitly set end value.
+        /// </param>
+        /// <param name="animationClock">
+        ///     The <see cref="AnimationClock"/> which can generate the <see cref="Clock.CurrentTime"/>
+        ///     or <see cref="Clock.CurrentProgress"/> value to be used by the
+        ///     animation to generate its output value.
+        /// </param>
+        /// <returns>The value this animation believes should be the current value for the property.</returns>
+        protected override Brush GetCurrentValueCore(Brush defaultOriginValue, Brush defaultDestinationValue, AnimationClock animationClock)
+        {
+            Brush origin = this.From ?? defaultOriginValue;
+            Brush destination = this.To ?? defaultDestinationValue;
+            this.ValidateTimelineValues(origin, destination);
+            throw new NotImplementedException();
+        }
+
+        private void ValidateTimelineValues(Brush origin, Brush destination)
+        {
+            this.ValidateThatBrushesAreNotNull(origin, destination);
+            this.ValidateThatBrushesHaveSameType(origin, destination);
+            this.ValidateThatBrushHasSupportedType(origin); // Call only once, since originType == destType.
+        }
+
+        private void ValidateThatBrushesAreNotNull(Brush origin, Brush destination)
+        {
+            if (origin == null || destination == null)
+                throw new InvalidOperationException(
+                    $"The {nameof(BrushAnimation)} requires an origin and a destination value. " +
+                    $"Ensure that both the {nameof(From)} and {nameof(To)} properties are set.");
+        }
+
+        private void ValidateThatBrushesHaveSameType(Brush origin, Brush destination)
+        {
+            if (origin.GetType() != destination.GetType())
+                throw new InvalidOperationException(
+                    $"The {nameof(BrushAnimation)} requires all brushes to be of the same type.");
+        }
+
+        private void ValidateThatBrushHasSupportedType(Brush brush)
+        {
+            Type myBrushType = brush.GetType();
+            if (myBrushType != typeof(SolidColorBrush) &&
+                myBrushType != typeof(LinearGradientBrush) &&
+                myBrushType != typeof(RadialGradientBrush))
+            {
+                throw new InvalidOperationException(
+                    $"The {nameof(BrushAnimation)} does not support brushes of type {myBrushType.FullName}.");
+            }
+        }
+        
+    }
+
+}
