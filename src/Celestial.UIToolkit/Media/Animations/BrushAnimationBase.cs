d09432b4d59d171cad7d3d440b85a6f9dd62877f
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-09 23:35:37 +0200

    Created a functional SolidColorBrushAnimation.

---
+++ src/Celestial.UIToolkit/Media/Animations/BrushAnimationBase.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Celestial.UIToolkit.Media.Animations
+{
+
+    /// <summary>
+    /// A base class for any animation that animates a <see cref="Brush"/>.
+    /// </summary>
+    public abstract class BrushAnimationBase : AnimationBase<Brush>
+    {
+        
+        /// <summary>
+        /// Identifies the <see cref="From"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty FromProperty = DependencyProperty.Register(
+            nameof(From), typeof(Brush), typeof(BrushAnimationBase), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="To"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ToProperty = DependencyProperty.Register(
+            nameof(To), typeof(Brush), typeof(BrushAnimationBase), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="EasingFunction"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty EasingFunctionProperty = DependencyProperty.Register(
+            nameof(EasingFunction), typeof(IEasingFunction), typeof(BrushAnimationBase), new PropertyMetadata(null));
+        
+        /// <summary>
+        /// Gets or sets a <see cref="Brush"/> which serves as the animation's
+        /// starting value.
+        /// </summary>
+        public Brush From
+        {
+            get { return (Brush)GetValue(FromProperty); }
+            set { SetValue(FromProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets a <see cref="Brush"/> which serves as the animation's ending value.
+        /// </summary>
+        public Brush To
+        {
+            get { return (Brush)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the easing function applied to this animation.
+        /// </summary>
+        public IEasingFunction EasingFunction
+        {
+            get { return (IEasingFunction)GetValue(EasingFunctionProperty); }
+            set { SetValue(EasingFunctionProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether the target property's current value 
+        /// should be added to this animation's starting value.
+        /// </summary>
+        public bool IsAdditive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that specifies whether the animation's value accumulates when it repeats.
+        /// </summary>
+        public bool IsCumulative { get; set; }
+
+        /// <summary>
+        ///     Returns the current value of the animation.
+        /// </summary>
+        /// <param name="defaultOriginValue">
+        ///     The origin value provided to the animation if the animation does not have its
+        ///     own start value. If this animation is the first in a composition chain it will
+        ///     be the base value of the property being animated; otherwise it will be the value
+        ///     returned by the previous animation in the chain.
+        /// </param>
+        /// <param name="defaultDestinationValue">
+        ///     The destination value provided to the animation if the animation does not have
+        ///     its own destination value.
+        /// </param>
+        /// <param name="animationClock">
+        ///     The <see cref="AnimationClock"/> which can generate the <see cref="Clock.CurrentTime"/>
+        ///     or <see cref="Clock.CurrentProgress"/> value to be used by the
+        ///     animation to generate its output value.
+        /// </param>
+        /// <returns>The value this animation believes should be the current value for the property.</returns>
+        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
+        {
+            Brush origin = this.From ?? defaultOriginValue as Brush;
+            Brush destination = this.To ?? defaultDestinationValue as Brush;
+            this.ValidateTimelineValues(origin, destination);
+
+            return base.GetCurrentValue(defaultOriginValue, defaultDestinationValue, animationClock);
+        }
+
+        private void ValidateTimelineValues(Brush origin, Brush destination)
+        {
+            this.ValidateThatBrushesAreNotNull(origin, destination);
+            this.ValidateThatBrushesHaveSameType(origin, destination);
+        }
+
+        private void ValidateThatBrushesAreNotNull(Brush origin, Brush destination)
+        {
+            if (origin == null || destination == null)
+                throw new InvalidOperationException(
+                    $"The brush animation requires an origin and a destination value. " +
+                    $"Ensure that both the {nameof(From)} and {nameof(To)} properties are set " +
+                    $"and that both of them are of type {nameof(Brush)}.");
+        }
+
+        private void ValidateThatBrushesHaveSameType(Brush origin, Brush destination)
+        {
+            if (origin.GetType() != destination.GetType())
+                throw new InvalidOperationException(
+                    $"The brush animation requires all brushes to be of the same type.");
+        }
+        
+    }
+    
+}
---
+++ src/Celestial.UIToolkit/Media/Animations/BrushAnimationMappers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Celestial.UIToolkit.Media.Animations
+{
+
+    /// <summary>
+    ///     Used internally by the <see cref="BrushAnimationBase"/> and its derivations.
+    /// 
+    ///     Serves as a base class for a mapper, which maps the properties of a
+    ///     <see cref="BrushAnimationBase"/> to another animation.
+    /// </summary>
+    /// <typeparam name="TAnimation">
+    ///     The type of the other animation.
+    /// </typeparam>
+    /// <typeparam name="TAnimated">
+    ///     The type which is being animated by the other animation.
+    /// </typeparam>
+    /// <remarks>
+    /// 
+    ///     The mapping is done, because a brush consists of several components, e.g. Opacity,
+    ///     Colors, ...
+    ///     Each of these components can be animated by one of WPF's animations.
+    ///     As a result, the brush animations simply delegate the animation down.
+    ///     Hence, these mappers are created.
+    ///     
+    /// </remarks>
+    internal abstract class BrushAnimationMapper<TAnimation, TAnimated> 
+        where TAnimation : AnimationTimeline
+    {
+
+        private bool _copiedFrozenValues;
+        private BrushAnimationBase _brushAnimation;
+        private TAnimation _animation;
+
+        public BrushAnimationMapper(BrushAnimationBase brushAnimation, Func<TAnimation> animationFactory)
+        {
+            _brushAnimation = brushAnimation ??
+                              throw new ArgumentNullException(nameof(brushAnimation));
+            _animation = animationFactory() ??
+                                throw new ArgumentException("The animation factory must not return null.",
+                                                            nameof(animationFactory));
+        }
+        
+        public TAnimated GetCurrentValue(TAnimated origin, TAnimated destination, AnimationClock animationClock)
+        {
+            this.UpdateAnimationProperties();
+            return (TAnimated)_animation.GetCurrentValue(
+                origin, destination, animationClock);
+        }
+
+        private void UpdateAnimationProperties()
+        {
+            if (_copiedFrozenValues) return;
+            _copiedFrozenValues = _brushAnimation.IsFrozen;
+
+            this.CopyBrushAnimationValues(
+                _brushAnimation, _animation);
+        }
+
+        protected abstract void CopyBrushAnimationValues(
+            BrushAnimationBase brushAnimation, TAnimation other);
+
+    }
+    
+    internal sealed class BrushColorAnimationMapper : BrushAnimationMapper<ColorAnimation, Color>
+    {
+        public BrushColorAnimationMapper(BrushAnimationBase brushAnimation) 
+            : base(brushAnimation, () => new ColorAnimation()) { }
+
+        protected override void CopyBrushAnimationValues(BrushAnimationBase brushAnim, ColorAnimation colorAnim)
+        {
+            colorAnim.EasingFunction = brushAnim.EasingFunction;
+            colorAnim.IsAdditive = brushAnim.IsAdditive;
+            colorAnim.IsCumulative = brushAnim.IsCumulative;
+            colorAnim.FillBehavior = brushAnim.FillBehavior;
+        }
+    }
+
+    internal sealed class BrushDoubleAnimationMapper : BrushAnimationMapper<DoubleAnimation, double>
+    {
+        public BrushDoubleAnimationMapper(BrushAnimationBase brushAnimation) 
+            : base(brushAnimation, () => new DoubleAnimation()) { }
+
+        protected override void CopyBrushAnimationValues(BrushAnimationBase brushAnim, DoubleAnimation doubleAnim)
+        {
+            doubleAnim.EasingFunction = brushAnim.EasingFunction;
+            doubleAnim.IsAdditive = brushAnim.IsAdditive;
+            doubleAnim.IsCumulative = brushAnim.IsCumulative;
+            doubleAnim.FillBehavior = brushAnim.FillBehavior;
+        }
+    }
+
+}
