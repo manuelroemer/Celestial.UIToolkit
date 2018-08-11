05835807c8db351a7ad589bb4f022c0e63e5bf89
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-11 19:17:14 +0200

    KeyFrameCollectionBase with IList<T> and IList methods added. Missing Freezable overrides.

---
+++ src/Celestial.UIToolkit/ExtendedFreezable.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows;
+
+namespace Celestial.UIToolkit
+{
+
+    /// <summary>
+    /// An abstract extension of the <see cref="Freezable"/> class,
+    /// providing additional quality-of-life methods for implementing custom freezable
+    /// objects.
+    /// </summary>
+    public abstract class ExtendedFreezable : Freezable
+    {
+
+        /// <summary>
+        /// Creates an artifical scope for reading properties around the specified
+        /// <paramref name="scopeAction"/>, by calling <see cref="Freezable.ReadPreamble"/> and then
+        /// executing the specified <paramref name="scopeAction"/>.
+        /// </summary>
+        /// <param name="scopeAction">
+        /// An <see cref="Action"/> to be executed.
+        /// </param>
+        protected void EnterReadScope(Action scopeAction)
+        {
+            this.ReadPreamble();
+            scopeAction?.Invoke();
+        }
+
+        /// <summary>
+        /// Creates an artificial scope for reading properties around the specified
+        /// <paramref name="scopeFunction"/>, by calling <see cref="Freezable.ReadPreamble"/> and then
+        /// executing the specified <paramref name="scopeFunction"/> and returning the result.
+        /// </summary>
+        /// <typeparam name="T">The type of the function's return value.</typeparam>
+        /// <param name="scopeFunction">
+        /// A function to be executed.
+        /// </param>
+        /// <returns>
+        /// The result of the function.
+        /// </returns>
+        /// <exception cref="ArgumentNullException" />
+        protected T EnterReadScope<T>(Func<T> scopeFunction)
+        {
+            if (scopeFunction == null) throw new ArgumentNullException(nameof(scopeFunction));
+            this.ReadPreamble();
+            return scopeFunction();
+        }
+
+        /// <summary>
+        /// Creates an artificial scope for writing properties around the specified
+        /// <paramref name="scopeAction"/>, by calling <see cref="Freezable.WritePreamble"/>,
+        /// executing the specified <paramref name="scopeAction"/> and then calling
+        /// <see cref="Freezable.WritePostscript"/>.
+        /// </summary>
+        /// <param name="scopeAction">
+        /// An <see cref="Action"/> to be executed.
+        /// </param>
+        protected void EnterWriteScope(Action scopeAction)
+        {
+            this.EnterWriteScope(scopeAction, false);
+        }
+
+        /// <summary>
+        /// Creates an artificial scope for writing properties around the specified
+        /// <paramref name="scopeAction"/>, by calling <see cref="Freezable.WritePreamble"/>,
+        /// executing the specified <paramref name="scopeAction"/> and then calling
+        /// <see cref="Freezable.WritePostscript"/> (if <paramref name="suppressWritePostscript"/> is
+        /// <c>false</c>).
+        /// </summary>
+        /// <param name="scopeAction">
+        /// An <see cref="Action"/> to be executed.
+        /// </param>
+        /// <param name="suppressWritePostscript">
+        /// A value indicating whether <see cref="Freezable.WritePostscript"/>
+        /// should be called after executing <paramref name="scopeAction"/>.
+        /// </param>
+        protected void EnterWriteScope(Action scopeAction, bool suppressWritePostscript)
+        {
+            this.WritePreamble();
+            scopeAction?.Invoke();
+            if (!suppressWritePostscript)
+                this.WritePostscript();
+        }
+
+        /// <summary>
+        /// Creates an artificial scope for writing properties around the specified
+        /// <paramref name="scopeFunction"/>, by calling <see cref="Freezable.WritePreamble"/>,
+        /// executing the specified <paramref name="scopeFunction"/>, calling
+        /// <see cref="Freezable.WritePostscript"/> and finally returning the 
+        /// <paramref name="scopeFunction"/>'s result.
+        /// </summary>
+        /// <param name="scopeFunction">
+        /// A function to be executed.
+        /// </param>
+        /// <returns>
+        /// The result of the function.
+        /// </returns>
+        /// <exception cref="ArgumentNullException" />
+        protected T EnterWriteScope<T>(Func<T> scopeFunction)
+        {
+            return this.EnterWriteScope(scopeFunction, false);
+        }
+
+        /// <summary>
+        /// Creates an artificial scope for writing properties around the specified
+        /// <paramref name="scopeFunction"/>, by calling <see cref="Freezable.WritePreamble"/>,
+        /// executing the specified <paramref name="scopeFunction"/>, calling
+        /// <see cref="Freezable.WritePostscript"/> (if <paramref name="suppressWritePostscript"/> is
+        /// <c>false</c>) and finally returning the <paramref name="scopeFunction"/>'s result.
+        /// </summary>
+        /// <param name="scopeFunction">
+        /// A function to be executed.
+        /// </param>
+        /// <param name="suppressWritePostscript">
+        /// A value indicating whether <see cref="Freezable.WritePostscript"/>
+        /// should be called after executing <paramref name="scopeFunction"/>.
+        /// </param>
+        /// <returns>
+        /// The result of the function.
+        /// </returns>
+        /// <exception cref="ArgumentNullException" />
+        protected T EnterWriteScope<T>(Func<T> scopeFunction, bool suppressWritePostscript)
+        {
+            if (scopeFunction == null) throw new ArgumentNullException(nameof(scopeFunction));
+            this.WritePreamble();
+            var result = scopeFunction();
+            if (!suppressWritePostscript)
+                this.WritePostscript();
+            return result;
+        }
+
+    }
+
+}
---
+++ src/Celestial.UIToolkit/Media/Animations/KeyFrameCollectionBase.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Celestial.UIToolkit.Media.Animations
+{
+
+    /// <summary>
+    /// An abstract base class, designed to hold <see cref="KeyFrameBase{T}"/> elements.
+    /// </summary>
+    /// <typeparam name="T">The type of the key frame's values.</typeparam>
+    public abstract class KeyFrameCollectionBase<T> : ExtendedFreezable, IList<KeyFrameBase<T>>, IList
+    {
+
+        private List<KeyFrameBase<T>> _keyFrames;
+
+        #region Freezable
+
+
+
+        #endregion
+
+        #region IList<T>
+
+        /// <summary>
+        /// Gets the total number of key frame elements inside this collection.
+        /// </summary>
+        public int Count => this.EnterReadScope(() => _keyFrames.Count);
+
+        /// <summary>
+        /// Gets a value indicating whether this collection is read-only,
+        /// which is the case, if it is frozen.
+        /// </summary>
+        public bool IsReadOnly => this.EnterReadScope(() => this.IsFrozen);
+
+        /// <summary>
+        /// Gets a value indicating whether the collection has a fixed size.
+        /// </summary>
+        public bool IsFixedSize => this.EnterReadScope(() => this.IsFrozen);
+
+        /// <summary>
+        /// Gets an object that can be used to synchronize access to the collection.
+        /// </summary>
+        public object SyncRoot => this.EnterReadScope(() => ((ICollection)_keyFrames).SyncRoot);
+
+        /// <summary>
+        /// Gets a value indicating whether access to the collection is synchronized (thread-safe).
+        /// </summary>
+        public bool IsSynchronized => this.EnterReadScope(() => this.IsFrozen ? true : this.Dispatcher != null);
+
+        /// <summary>
+        /// Gets or sets the key frame at the specified <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The zero-based index of the key frame to get or set.</param>
+        /// <returns>The key frame at the specified <paramref name="index"/>.</returns>
+        public KeyFrameBase<T> this[int index]
+        {
+            get
+            {
+                return this.EnterReadScope(() => _keyFrames[index]);
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                this.EnterWriteScope(() =>
+                {
+                    this.OnFreezablePropertyChanged(_keyFrames[index], value);
+                    _keyFrames[index] = value;
+                });
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the collection contains a specific key frame.
+        /// </summary>
+        /// <param name="item">The key frame to locate in the collection.</param>
+        /// <returns>
+        /// <c>true</c> if the key frame is found in the collection;
+        /// <c>false</c> if not.
+        /// </returns>
+        public bool Contains(KeyFrameBase<T> item)
+        {
+            return this.EnterReadScope(() => _keyFrames.Contains(item));
+        }
+
+        /// <summary>
+        /// Copies the key frames of the collection into an array,
+        /// starting at <paramref name="arrayIndex"/>.
+        /// </summary>
+        /// <param name="array">
+        /// The one-dimensional array that is the destination of the
+        /// key frames copied from the collection.
+        /// </param>
+        /// <param name="arrayIndex">
+        /// The zero-based index in the array, at which copying begins.
+        /// </param>
+        public void CopyTo(KeyFrameBase<T>[] array, int arrayIndex)
+        {
+            this.EnterReadScope(() => _keyFrames.CopyTo(array, arrayIndex));
+        }
+
+        /// <summary>
+        /// Determines the index of the specified key frame in the collection.
+        /// </summary>
+        /// <param name="item">The key frame to locate in this collection.</param>
+        /// <returns>The index of the key frame if found in the collection; -1 otherwise.</returns>
+        public int IndexOf(KeyFrameBase<T> item)
+        {
+            return this.EnterReadScope(() => _keyFrames.IndexOf(item));
+        }
+
+        /// <summary>
+        /// Adds a key frame to the collection.
+        /// </summary>
+        /// <param name="item">The key frame to be added to the collection.</param>
+        public void Add(KeyFrameBase<T> item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            this.EnterWriteScope(() =>
+            {
+                this.OnFreezablePropertyChanged(null, item);
+                _keyFrames.Add(item);
+            });
+        }
+
+        /// <summary>
+        /// Inserts a key frame to to the collection at the specified <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The zero-based index at which the key frame should be inserted.</param>
+        /// <param name="item">The key frame to insert into the collection.</param>
+        public void Insert(int index, KeyFrameBase<T> item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            if (index < 0 || index > _keyFrames.Count - 1)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            this.EnterWriteScope(() =>
+            {
+                this.OnFreezablePropertyChanged(null, item);
+                _keyFrames.Insert(index, item);
+            });
+        }
+
+        /// <summary>
+        /// Inserts a key frame at the end of the collection and returns the index,
+        /// at which the key frame was inserted.
+        /// </summary>
+        /// <param name="item">The key frame to be inserted.</param>
+        /// <returns>The index at which the key frame was inserted.</returns>
+        public int InsertEnd(KeyFrameBase<T> item)
+        {
+            return this.EnterReadScope(() =>
+            {
+                int index = this.Count - 1;
+                this.Insert(index, item);
+                return index;
+            });
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specified key frame from the collection.
+        /// </summary>
+        /// <param name="item">The key frame to be removed from the collection.</param>
+        /// <returns>
+        /// <c>true</c> if the key frame was successfully removed from the collection;
+        /// <c>false</c> if not.
+        /// </returns>
+        public bool Remove(KeyFrameBase<T> item)
+        {
+            return this.EnterWriteScope(() =>
+            {
+                if (_keyFrames.Contains(item))
+                {
+                    this.OnFreezablePropertyChanged(item, null);
+                    return _keyFrames.Remove(item);
+                }
+                else
+                {
+                    return false;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Removes the key frame at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to remove.</param>
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > _keyFrames.Count - 1)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            this.EnterWriteScope(() =>
+            {
+                this.OnFreezablePropertyChanged(_keyFrames[index], null);
+                _keyFrames.RemoveAt(index);
+            });
+        }
+
+        /// <summary>
+        /// Removes all key frames from the collection.
+        /// </summary>
+        public void Clear()
+        {
+            this.EnterWriteScope(() =>
+            {
+                for (int i = 0; i < _keyFrames.Count; i++)
+                {
+                    this.OnFreezablePropertyChanged(_keyFrames[i], null);
+                }
+                _keyFrames.Clear();
+            });
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        public IEnumerator<KeyFrameBase<T>> GetEnumerator()
+        {
+            return this.EnterReadScope(() => _keyFrames.GetEnumerator());
+        }
+
+        #endregion
+
+        #region IList
+
+        object IList.this[int index]
+        {
+            get => this[index];
+            set => this[index] = (KeyFrameBase<T>)value;
+        }
+
+        int IList.Add(object value) => this.InsertEnd((KeyFrameBase<T>)value);
+
+        bool IList.Contains(object value) => this.Contains((KeyFrameBase<T>)value);
+
+        void IList.Clear() => this.Clear();
+
+        int IList.IndexOf(object value) => this.IndexOf((KeyFrameBase<T>)value);
+
+        void IList.Insert(int index, object value) => this.Insert(index, (KeyFrameBase<T>)value);
+
+        void IList.Remove(object value) => this.Remove((KeyFrameBase<T>)value);
+
+        void IList.RemoveAt(int index) => this.RemoveAt(index);
+
+        void ICollection.CopyTo(Array array, int index) => this.CopyTo((KeyFrameBase<T>[])array, index);
+
+        #endregion
+
+    }
+
+}
