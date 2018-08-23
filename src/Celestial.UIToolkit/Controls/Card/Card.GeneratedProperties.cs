58bfc5f4fcbcdf16e1c15a896bacabd77d10848e
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-23 23:16:18 +0200

    Moved the Card files into their own folder, matching the NavigationView.

--- src/Celestial.UIToolkit/Controls/Card/Card.GeneratedProperties.cs
+++ src/Celestial.UIToolkit/Controls/Card/Card.GeneratedProperties.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 // This file is auto-generated.
