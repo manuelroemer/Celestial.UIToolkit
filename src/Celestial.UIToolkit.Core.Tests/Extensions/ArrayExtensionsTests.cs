8e81732bec8a6d0acf3aca78d3ee7b5b7b1aa016
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-23 14:59:17 +0200

    Added Enumerable and Array Extensions tests.

---
+++ src/Celestial.UIToolkit.Core.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using Celestial.UIToolkit.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Celestial.UIToolkit.Tests.Extensions
+{
+
+    public class ArrayExtensionsTests
+    {
+
+        [Theory]
+        [ClassData(typeof(SegmentTestDataSource))]
+        public void GetGroupSegmentsTest(int[] array, IEnumerable<ArraySegment<int>> expectedSegments)
+        {
+            Func<int, bool> IsEven = (num) => num % 2 == 0;
+            var groupSegments = array.GetGroupSegments(IsEven);
+            Assert.Equal(expectedSegments, groupSegments);
+        }
+
+        private class SegmentTestDataSource : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new int[0],
+                    new ArraySegment<int>[0]
+                };
+
+                yield return new object[]
+                {
+                    new int[] { 1, 1, 1, 1, 1 },
+                    new ArraySegment<int>[0]
+                };
+
+                int[] evenOnlyArray = new int[] { 2, 2, 2, 2, 2 };
+                yield return new object[]
+                {
+                    evenOnlyArray,
+                    new ArraySegment<int>[]
+                    {
+                        new ArraySegment<int>(evenOnlyArray, 0, evenOnlyArray.Length)
+                    }
+                };
+
+                int[] startEndSegmentsArray = new int[] { 2, 2, 2, 1, 2, 2, 2 };
+                yield return new object[]
+                {
+                    startEndSegmentsArray,
+                    new ArraySegment<int>[]
+                    {
+                        new ArraySegment<int>(startEndSegmentsArray, 0, 3),
+                        new ArraySegment<int>(startEndSegmentsArray, 4, 3),
+                    }
+                };
+
+                int[] middleSegmentArray = new int[] { 1, 2, 1 };
+                yield return new object[]
+                {
+                    middleSegmentArray,
+                    new ArraySegment<int>[]
+                    {
+                        new ArraySegment<int>(middleSegmentArray, 1, 1)
+                    }
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+    }
+
+}
---
+++ src/Celestial.UIToolkit.Core.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,84 @@
+﻿using Celestial.UIToolkit.Extensions;
+using System;
+using System.Collections.ObjectModel;
+using Xunit;
+
+namespace Celestial.UIToolkit.Tests.Extensions
+{
+
+    public class EnumerableExtensionsTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, -1)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 0)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 1)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 2)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 3)]
+        public void ElementAfterReturnsCorrectValue(int[] data, int index)
+        {
+            Assert.Equal(
+                data[index + 1],
+                data.ElementAfter(index));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 1)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 2)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 3)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 4)]
+        [InlineData(new int[] { 0, 1, 2, 3, 4 }, 5)]
+        public void ElementBeforeReturnsCorrectValue(int[] data, int index)
+        {
+            Assert.Equal(
+                data[index - 1],
+                data.ElementBefore(index));
+        }
+
+        [Fact]
+        public void ElementAfterOrDefaultReturnsDefaultValue()
+        {
+            Assert.Equal(
+                default(int),
+                new int[0].ElementAfterOrDefault(0));
+        }
+
+        [Fact]
+        public void ElementBeforeOrDefaultReturnsDefaultValue()
+        {
+            Assert.Equal(
+                default(int),
+                new int[0].ElementBeforeOrDefault(0));
+        }
+
+        [Fact]
+        public void AddRangeAddsElements()
+        {
+            var baseCollection = new Collection<int>();
+            var nums = new int[] { 1, 2, 3 };
+
+            baseCollection.AddRange(nums);
+            Assert.Equal(nums, baseCollection);
+        }
+
+        [Fact]
+        public void AddRangeThrowsForNullCollection()
+        {
+            var collection = new Collection<int>();
+            var ex = Record.Exception(() => collection.AddRange(null));
+            Assert.IsType<ArgumentNullException>(ex);
+        }
+
+        [Fact]
+        public void RemoveAllRemovesAllItems()
+        {
+            var collection = new Collection<int>() { 0, 1, 2, 3, 4, 5, 6 };
+            Func<int, bool> IsEven = (num) => num % 2 == 0;
+
+            collection.RemoveAll(IsEven);
+            Assert.DoesNotContain(collection, (num) => IsEven(num));
+        }
+
+    }
+
+}
