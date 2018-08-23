fbed9492b07631502785dc9f59f1bd71c5e54053
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-23 15:11:15 +0200

    Added array segment extension test

---
+++ src/Celestial.UIToolkit.Core.Tests/Extensions/ArraySegmentExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Celestial.UIToolkit.Extensions;
+using System;
+using Xunit;
+
+namespace Celestial.UIToolkit.Tests.Extensions
+{
+
+    public class ArraySegmentExtensionsTests
+    {
+
+        [Fact]
+        public void IncludesElementAtReturnsCorrectValue()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4 };
+            var segment = new ArraySegment<int>(array, 1, 4);
+
+            Assert.False(segment.IncludesElementAt(0));
+            for (int i = segment.Offset; i < segment.Offset + segment.Count; i++)
+            {
+                Assert.True(segment.IncludesElementAt(i));
+            }
+        }
+        
+    }
+
+}
