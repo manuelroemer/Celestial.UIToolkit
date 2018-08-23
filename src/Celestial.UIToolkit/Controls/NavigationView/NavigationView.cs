b44efafe12ffa749fd163c7bd4e1e6ca7c424aea
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-23 22:44:40 +0200

    Created a folder structure for the NavigationView control and added a DisplayMode property.

---
+++ src/Celestial.UIToolkit/Controls/NavigationView/NavigationView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Celestial.UIToolkit.Controls
+{
+
+    /// <summary>
+    ///     A control which provides a central navigation structure to an application by providing
+    ///     a pane for navigation commands and a central place for the content which is the current
+    ///     navigation target.
+    /// </summary>
+    /// <remarks>
+    ///     This control is built after the UWP navigation control.
+    ///     As a result, the control behaves similarly and provides the same kind of properties.
+    ///     While feature-parity is not guaranteed, having a closer look at the official 
+    ///     documentation by Microsoft will be a good idea.
+    /// </remarks>
+    public partial class NavigationView : HeaderedContentControl
+    {
+
+        static NavigationView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(NavigationView), new FrameworkPropertyMetadata(typeof(NavigationView)));
+        }
+
+    }
+
+}
---
+++ src/Celestial.UIToolkit/Controls/NavigationView/NavigationViewDisplayMode.cs
@@ -0,0 +1,31 @@
+﻿namespace Celestial.UIToolkit.Controls
+{
+
+    /// <summary>
+    /// Defines the different display modes of a <see cref="NavigationView"/> control.
+    /// </summary>
+    public enum NavigationViewDisplayMode
+    {
+
+        /// <summary>
+        /// The <see cref="NavigationView"/> displays only the minimal amount of elements,
+        /// which happens to be the menu button.
+        /// With this button, the pane can be shown and hidden.
+        /// </summary>
+        Minimal,
+
+        /// <summary>
+        /// The <see cref="NavigationView"/> shows a small strip which can be expanded via the
+        /// menu button.
+        /// </summary>
+        Compact,
+
+        /// <summary>
+        /// The <see cref="NavigationView"/> is fully expanded, meaning that the pane stays open
+        /// the whole time.
+        /// </summary>
+        Expanded
+
+    }
+
+}
