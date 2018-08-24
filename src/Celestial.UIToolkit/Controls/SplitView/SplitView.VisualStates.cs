b2bc95ee2923d6af10107f02fb1e05bfada7ebf2
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-24 15:32:49 +0200

    Added pre-emptive visual states to the split view.

--- src/Celestial.UIToolkit/Controls/SplitView/SplitView.VisualStates.cs
+++ src/Celestial.UIToolkit/Controls/SplitView/SplitView.VisualStates.cs
@@ -10,12 +10,22 @@ namespace Celestial.UIToolkit.Controls
 
     [TemplateVisualState(Name = PaneOpenVisualState, GroupName = PaneToggleStatesVisualStateGroup)]
     [TemplateVisualState(Name = PaneClosedVisualState, GroupName = PaneToggleStatesVisualStateGroup)]
+    [TemplateVisualState(Name = OverlayVisualState, GroupName = DisplayModesVisualStateGroup)]
+    [TemplateVisualState(Name = InlineVisualState, GroupName = DisplayModesVisualStateGroup)]
+    [TemplateVisualState(Name = CompactOverlayVisualState, GroupName = DisplayModesVisualStateGroup)]
+    [TemplateVisualState(Name = CompactInlineVisualState, GroupName = DisplayModesVisualStateGroup)]
     public partial class SplitView
     {
 
         internal const string PaneToggleStatesVisualStateGroup = "PaneToggleStates";
         internal const string PaneOpenVisualState = "Open";
         internal const string PaneClosedVisualState = "Closed";
+
+        internal const string DisplayModesVisualStateGroup = "DisplayModes";
+        internal const string OverlayVisualState = "Overlay";
+        internal const string InlineVisualState = "Inline";
+        internal const string CompactOverlayVisualState = "CompactOverlay";
+        internal const string CompactInlineVisualState = "CompactInline";
 
         private void EnterCurrentPaneToggleVisualState()
         {
@@ -26,11 +36,37 @@ namespace Celestial.UIToolkit.Controls
         {
             if (IsPaneOpen)
             {
-                VisualStateManager.GoToState(this, PaneOpenVisualState, true);
+                VisualStateManager.GoToState(this, PaneOpenVisualState, useTransitions);
             }
             else
             {
-                VisualStateManager.GoToState(this, PaneClosedVisualState, true);
+                VisualStateManager.GoToState(this, PaneClosedVisualState, useTransitions);
+            }
+        }
+
+        private void EnterCurrentDisplayModeVisualState()
+        {
+            EnterCurrentDisplayModeVisualState(true);
+        }
+
+        private void EnterCurrentDisplayModeVisualState(bool useTransitions)
+        {
+            switch (DisplayMode)
+            {
+                case SplitViewDisplayMode.Overlay:
+                    VisualStateManager.GoToState(this, OverlayVisualState, useTransitions);
+                    break;
+                case SplitViewDisplayMode.Inline:
+                    VisualStateManager.GoToState(this, InlineVisualState, useTransitions);
+                    break;
+                case SplitViewDisplayMode.CompactOverlay:
+                    VisualStateManager.GoToState(this, CompactOverlayVisualState, useTransitions);
+                    break;
+                case SplitViewDisplayMode.CompactInline:
+                    VisualStateManager.GoToState(this, CompactInlineVisualState, useTransitions);
+                    break;
+                default:
+                    throw new NotImplementedException();
             }
         }
 
