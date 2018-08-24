b8caa4b7be0ae9796a8a3da21be1ac4a04365f0e
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-24 20:42:41 +0200

    Added Pane Content properties.

--- src/Celestial.UIToolkit/Controls/SplitView/SplitView.Properties.cs
+++ src/Celestial.UIToolkit/Controls/SplitView/SplitView.Properties.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace Celestial.UIToolkit.Controls
@@ -130,6 +133,198 @@ namespace Celestial.UIToolkit.Controls
             get { return (Brush)GetValue(PaneBackgroundProperty); }
             set { SetValue(PaneBackgroundProperty, value); }
         }
+
+
+        private static readonly DependencyPropertyKey HasPanePropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                nameof(HasPane),
+                typeof(bool),
+                typeof(SplitView),
+                new PropertyMetadata(false));
+
+        /// <summary>
+        /// Identifies the <see cref="HasPane"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HasPaneProperty =
+            HasPanePropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Identifies the <see cref="Pane"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PaneProperty =
+            DependencyProperty.Register(
+                nameof(Pane),
+                typeof(object),
+                typeof(SplitView),
+                new PropertyMetadata(
+                    null,
+                    Pane_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="PaneTemplate"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PaneTemplateProperty =
+            DependencyProperty.Register(
+                nameof(PaneTemplate),
+                typeof(DataTemplate),
+                typeof(SplitView),
+                new PropertyMetadata(
+                    null,
+                    PaneTemplate_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="PaneTemplateSelector"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PaneTemplateSelectorProperty =
+            DependencyProperty.Register(
+                nameof(PaneTemplateSelector),
+                typeof(DataTemplateSelector),
+                typeof(SplitView),
+                new PropertyMetadata(
+                    null,
+                    PaneTemplateSelector_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="PaneStringFormat"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PaneStringFormatProperty =
+            DependencyProperty.Register(
+                nameof(PaneStringFormat),
+                typeof(string),
+                typeof(SplitView),
+                new PropertyMetadata(
+                    null,
+                    PaneStringFormat_Changed));
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="Pane"/> property of this
+        /// <see cref="SplitView"/> holds any content.
+        /// </summary>
+        [Bindable(false), Browsable(false)]
+        public bool HasPane
+        {
+            get { return (bool)GetValue(HasPaneProperty); }
+            private set { SetValue(HasPanePropertyKey, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the content which is being displayed in the pane of the
+        /// <see cref="SplitView"/>.
+        /// </summary>
+        [Bindable(true), Category("Content")]
+        [Localizability(LocalizationCategory.Label)]
+        public object Pane
+        {
+            get { return (object)GetValue(PaneProperty); }
+            set { SetValue(PaneProperty, value); }
+        }
+        
+        /// <summary>
+        /// Gets or sets a <see cref="DataTemplate"/> to be used for displaying the 
+        /// <see cref="Pane"/> content.
+        /// </summary>
+        [Bindable(true), Category("Content")]
+        public DataTemplate PaneTemplate
+        {
+            get { return (DataTemplate)GetValue(PaneTemplateProperty); }
+            set { SetValue(PaneTemplateProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="DataTemplateSelector"/> which enables an application writer
+        /// to provide custom template-selection logic for the <see cref="Pane"/> content.
+        /// </summary>
+        [Bindable(true), Category("Content")]
+        public DataTemplateSelector PaneTemplateSelector
+        {
+            get { return (DataTemplateSelector)GetValue(PaneTemplateSelectorProperty); }
+            set { SetValue(PaneTemplateSelectorProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a composite string that specifies how to format the <see cref="Pane"/>
+        /// content property, if it is displayed as a string.
+        /// </summary>
+        [Bindable(true), Category("Content")]
+        public string PaneStringFormat
+        {
+            get { return (string)GetValue(PaneStringFormatProperty); }
+            set { SetValue(PaneStringFormatProperty, value); }
+        }
+
+        private static void Pane_Changed(
+            DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = (SplitView)d;
+            self.HasPane = e.NewValue != null;
+            self.OnPaneChanged(e.OldValue, e.NewValue);
+        }
+
+        private static void PaneTemplate_Changed(
+            DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = (SplitView)d;
+            self.OnPaneTemplateChanged((DataTemplate)e.OldValue, (DataTemplate)e.NewValue);
+        }
+
+        private static void PaneTemplateSelector_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = (SplitView)d;
+            self.OnPaneTemplateSelectorChanged(
+                (DataTemplateSelector)e.OldValue, (DataTemplateSelector)e.NewValue);
+        }
+
+        private static void PaneStringFormat_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = (SplitView)d;
+            self.OnPaneStringFormatChanged((string)e.OldValue, (string)e.NewValue);
+        }
+
+        /// <summary>
+        /// Called when the <see cref="Pane"/> property was changed.
+        /// </summary>
+        /// <param name="oldValue">The old value of the <see cref="Pane"/> property.</param>
+        /// <param name="newValue">The new value of the <see cref="Pane"/> property.</param>
+        protected virtual void OnPaneChanged(object oldValue, object newValue)
+        {
+            RemoveLogicalChild(oldValue);
+            AddLogicalChild(newValue);
+        }
+
+        /// <summary>
+        /// Called when the <see cref="PaneTemplate"/> property was changed.
+        /// </summary>
+        /// <param name="oldTemplate">The old value of the <see cref="PaneTemplate"/> property.</param>
+        /// <param name="newTemplate">The new value of the <see cref="PaneTemplate"/> property.</param>
+        protected virtual void OnPaneTemplateChanged(
+            DataTemplate oldTemplate, DataTemplate newTemplate)
+        { }
+
+        /// <summary>
+        /// Called when the <see cref="PaneTemplateSelector"/> property was changed.
+        /// </summary>
+        /// <param name="oldSelector">
+        /// The old value of the <see cref="PaneTemplateSelector"/> property.
+        /// </param>
+        /// <param name="newSelector">
+        /// The new value of the <see cref="PaneTemplateSelector"/> property.
+        /// </param>
+        protected virtual void OnPaneTemplateSelectorChanged(
+            DataTemplateSelector oldSelector, DataTemplateSelector newSelector)
+        { }
+
+        /// <summary>
+        /// Called when the <see cref="PaneStringFormat"/> property was changed.
+        /// </summary>
+        /// <param name="oldStringFormat">
+        /// The old value of the <see cref="PaneStringFormat"/> property.
+        /// </param>
+        /// <param name="newStringFormat">
+        /// The new value of the <see cref="PaneStringFormat"/> property.
+        /// </param>
+        protected virtual void OnPaneStringFormatChanged(
+            string oldStringFormat, string newStringFormat)
+        { }
 
     }
 
