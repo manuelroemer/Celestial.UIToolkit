692c8477353c845bae4b5e86e2e0c17593db0616
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-25 18:54:25 +0200

    Implemented/Added the Header property/content.

---
+++ src/Celestial.UIToolkit/Controls/NavigationView/NavigationView.HeaderProperties.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Celestial.UIToolkit.Controls
+{
+
+    public partial class NavigationView
+    {
+        
+        /// <summary>
+        /// Identifies the <see cref="IsHeaderVisible"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsHeaderVisibleProperty =
+            DependencyProperty.Register(
+                nameof(IsHeaderVisible),
+                typeof(bool),
+                typeof(NavigationView),
+                new PropertyMetadata(true));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the <see cref="HeaderedContentControl.Header"/>
+        /// content of this <see cref="NavigationView"/> is currently visible.
+        /// </summary>
+        public bool IsHeaderVisible
+        {
+            get { return (bool)GetValue(IsHeaderVisibleProperty); }
+            set { SetValue(IsHeaderVisibleProperty, value); }
+        }
+        
+    }
+
+}
