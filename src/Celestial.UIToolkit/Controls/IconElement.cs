ef6fc69a584b473e0275063f55f01aee46e95731
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-30 14:18:00 +0200

    Made the Icon's foreground property inheritable.

--- src/Celestial.UIToolkit/Controls/IconElement.cs
+++ src/Celestial.UIToolkit/Controls/IconElement.cs
@@ -15,14 +15,15 @@ namespace Celestial.UIToolkit.Controls
         /// <summary>
         /// Identifies the <see cref="Foreground"/> dependency property.
         /// </summary>
-        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register(
+        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.RegisterAttached(
             nameof(Foreground), 
             typeof(Brush), 
             typeof(IconElement), 
             new FrameworkPropertyMetadata(
                 SystemColors.ControlTextBrush,
+                FrameworkPropertyMetadataOptions.Inherits |
                 FrameworkPropertyMetadataOptions.AffectsRender));
-
+        
         /// <summary>
         /// Gets or sets a <see cref="Brush"/> which identifies the icon's color.
         /// </summary>
@@ -31,6 +32,37 @@ namespace Celestial.UIToolkit.Controls
             get { return (Brush)GetValue(ForegroundProperty); }
             set { SetValue(ForegroundProperty, value); }
         }
+
+        /// <summary>
+        /// Gets the value of the <see cref="ForegroundProperty"/> attached dependency property
+        /// for a given <see cref="DependencyObject"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="DependencyObject"/> for which the local value of the
+        /// <see cref="ForegroundProperty"/> attached dependency property
+        /// should be retrieved.
+        /// </param>
+        /// <returns>
+        /// The local value of the <see cref="ForegroundProperty"/> attached dependency property,
+        /// which is of type <see cref="Brush"/>.
+        /// </returns>
+        public static Brush GetForeground(DependencyObject obj) =>
+            (Brush)obj.GetValue(ForegroundProperty);
+
+        /// <summary>
+        /// Sets the value of the <see cref="ForegroundProperty"/> attached dependency property
+        /// for a given <see cref="DependencyObject"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="DependencyObject"/> for which the local value of the
+        /// <see cref="ForegroundProperty"/> attached dependency property
+        /// should be set.
+        /// </param>
+        /// <param name="value">
+        /// The new value for the dependency property.
+        /// </param>
+        public static void SetForeground(DependencyObject obj, Brush value) =>
+            obj.SetValue(ForegroundProperty, value);
 
     }
 
