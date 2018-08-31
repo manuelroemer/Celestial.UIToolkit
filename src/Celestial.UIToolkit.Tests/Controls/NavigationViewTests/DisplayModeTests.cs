8338427325fdb05793df92d72854e84ca977affd
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-08-31 13:06:51 +0200

    Refactored old NavView tests and added new ones for the adaptive behavior.

--- src/Celestial.UIToolkit.Tests/Controls/NavigationViewTests/DisplayModeTests.cs
+++ src/Celestial.UIToolkit.Tests/Controls/NavigationViewTests/DisplayModeTests.cs
@@ -1,9 +1,8 @@
 ﻿using Celestial.UIToolkit.Controls;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
 using Xunit;
 
 namespace Celestial.UIToolkit.Tests.Controls.NavigationViewTests
@@ -12,41 +11,61 @@ namespace Celestial.UIToolkit.Tests.Controls.NavigationViewTests
     public class DisplayModeTests
     {
 
+        [WpfTheory]
+        [InlineData(100, 200, 50, NavigationViewDisplayMode.Minimal)]   // Default threshold ranges
+        [InlineData(100, 200, 150, NavigationViewDisplayMode.Compact)]  //  -> (Compact < Expanded)
+        [InlineData(100, 200, 300, NavigationViewDisplayMode.Expanded)]
+        [InlineData(200, 100, 50, NavigationViewDisplayMode.Minimal)]   // Compact > Expanded
+        [InlineData(200, 100, 150, NavigationViewDisplayMode.Expanded)]
+        [InlineData(200, 100, 300, NavigationViewDisplayMode.Expanded)]
+        [InlineData(200, 200, 50, NavigationViewDisplayMode.Minimal)]   // Compact == Expanded
+        [InlineData(200, 200, 200, NavigationViewDisplayMode.Expanded)]
+        public void DisplayModeIsAdaptive(
+            double compactThresholdWidth,
+            double expandedThresholdWidth,
+            double newWidth,
+            NavigationViewDisplayMode expectedDisplayMode)
+        {
+            var navView = new NavigationView()
+            {
+                CompactModeThresholdWidth = compactThresholdWidth,
+                ExpandedModeThresholdWidth = expandedThresholdWidth
+            };
+
+            navView.ChangeActualSizeAndRaiseEvent(newWidth, newWidth);
+            Assert.Equal(expectedDisplayMode, navView.DisplayMode);
+        }
+
         [WpfFact]
         public void FiresDisplayModeChangedEvent()
         {
             var navView = new NavigationView();
-            bool eventFired = false;
-            navView.DisplayModeChanged += DisplayMode_Changed;
 
-            navView.DisplayMode = NavigationViewDisplayMode.Compact;
-            Assert.True(eventFired);
-
-            void DisplayMode_Changed(object sender, NavigationViewDisplayModeChangedEventArgs e)
-            {
-                navView.DisplayModeChanged -= DisplayMode_Changed;
-                eventFired = true;
-            }
+            Assert.Raises<NavigationViewDisplayModeChangedEventArgs>(
+                (handler) => navView.DisplayModeChanged += handler,
+                (handler) => navView.DisplayModeChanged -= handler,
+                () => navView.DisplayMode = NavigationViewDisplayMode.Compact);
         }
 
-        [WpfFact]
-        public void DisplayModeChangedCarriesOldAndNewValues()
+        [WpfTheory]
+        [InlineData(NavigationViewDisplayMode.Minimal, NavigationViewDisplayMode.Compact)]
+        [InlineData(NavigationViewDisplayMode.Compact, NavigationViewDisplayMode.Expanded)]
+        [InlineData(NavigationViewDisplayMode.Expanded, NavigationViewDisplayMode.Minimal)]
+        public void DisplayModeChangedCarriesOldAndNewValues(
+            NavigationViewDisplayMode oldDisplayMode,
+            NavigationViewDisplayMode newDisplayMode)
         {
             var navView = new NavigationView();
-            NavigationViewDisplayModeChangedEventArgs eventArgs = null;
-            navView.DisplayModeChanged += DisplayMode_Changed;
-
-            navView.DisplayMode = NavigationViewDisplayMode.Expanded;
-
-            Assert.NotNull(eventArgs);
-            Assert.Equal(NavigationViewDisplayMode.Minimal, eventArgs.OldDisplayMode);
-            Assert.Equal(NavigationViewDisplayMode.Expanded, eventArgs.NewDisplayMode);
-
-            void DisplayMode_Changed(object sender, NavigationViewDisplayModeChangedEventArgs e)
-            {
-                navView.DisplayModeChanged -= DisplayMode_Changed;
-                eventArgs = e;
-            }
+            navView.DisplayMode = oldDisplayMode;
+            
+            var raisedEvent = Assert.Raises<NavigationViewDisplayModeChangedEventArgs>(
+                (handler) => navView.DisplayModeChanged += handler,
+                (handler) => navView.DisplayModeChanged -= handler,
+                () => navView.DisplayMode = newDisplayMode);
+            var eventArgs = raisedEvent.Arguments;
+            
+            Assert.Equal(oldDisplayMode, eventArgs.OldDisplayMode);
+            Assert.Equal(newDisplayMode, eventArgs.NewDisplayMode);
         }
 
     }
