eb59eee97c8d37d598645b096237cb31bceca5fb
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-09-01 20:21:36 +0200

    DropShadowPanel, similar to the Win UI Toolkit.

---
+++ src/Celestial.UIToolkit/Controls/DropShadowPanel/DropShadowPanel.Properties.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace Celestial.UIToolkit.Controls
+{
+
+    public partial class DropShadowPanel
+    {
+		
+        private static readonly DependencyPropertyKey DropShadowEffectPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                nameof(DropShadowEffect),
+                typeof(DropShadowEffect),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="DropShadowEffect"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty DropShadowEffectProperty =
+            DropShadowEffectPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowColor"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowColorProperty =
+            DependencyProperty.Register(
+                nameof(ShadowColor),
+                typeof(Color),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(
+					Colors.Black,
+					Shadow_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="ShadowOpacity"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShadowOpacityProperty =
+            DependencyProperty.Register(
+                nameof(ShadowOpacity),
+                typeof(double),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(
+                    1d,
+                    Shadow_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="BlurRadius"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty BlurRadiusProperty =
+            DependencyProperty.Register(
+                nameof(BlurRadius),
+                typeof(double),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(
+                    5d, Shadow_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="OffsetX"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty OffsetXProperty =
+            DependencyProperty.Register(
+                nameof(OffsetX),
+                typeof(double),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(
+                    0d,
+                    Shadow_Changed));
+
+        /// <summary>
+        /// Identifies the <see cref="OffsetY"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty OffsetYProperty =
+            DependencyProperty.Register(
+                nameof(OffsetY),
+                typeof(double),
+                typeof(DropShadowPanel),
+                new PropertyMetadata(
+                    0d,
+                    Shadow_Changed));
+
+        /// <summary>
+        /// Gets the calculated <see cref="DropShadowEffect"/> which is applied to the
+        /// panel's content.
+        /// </summary>
+        public DropShadowEffect DropShadowEffect
+        {
+            get { return (DropShadowEffect)GetValue(DropShadowEffectProperty); }
+            private set { SetValue(DropShadowEffectPropertyKey, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the shadow's color.
+        /// </summary>
+        public Color ShadowColor
+        {
+            get { return (Color)GetValue(ShadowColorProperty); }
+            set { SetValue(ShadowColorProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the opacity of the drop shadow.
+        /// </summary>
+        public double ShadowOpacity
+        {
+            get { return (double)GetValue(ShadowOpacityProperty); }
+            set { SetValue(ShadowOpacityProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that indicates the radius of the shadow's blur effect.
+        /// </summary>
+        public double BlurRadius
+        {
+            get { return (double)GetValue(BlurRadiusProperty); }
+            set { SetValue(BlurRadiusProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the X-Offset of the drop shadow.
+        /// </summary>
+        public double OffsetX
+        {
+            get { return (double)GetValue(OffsetXProperty); }
+            set { SetValue(OffsetXProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Y-Offset of the drop shadow.
+        /// </summary>
+        public double OffsetY
+        {
+            get { return (double)GetValue(OffsetYProperty); }
+            set { SetValue(OffsetYProperty, value); }
+        }
+
+    }
+
+}
---
+++ src/Celestial.UIToolkit/Controls/DropShadowPanel/DropShadowPanel.cs
@@ -0,0 +1,120 @@
+﻿using Celestial.UIToolkit.Theming;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Effects;
+using static System.Math;
+
+namespace Celestial.UIToolkit.Controls
+{
+
+    public partial class DropShadowPanel : ContentControl
+    {
+
+        private DropShadowEffect _currentEffect;
+
+        /// <summary>
+        /// Gets the <see cref="OffsetX"/> and <see cref="OffsetY"/> coordinates
+        /// as a <see cref="Vector"/>.
+        /// </summary>
+        public Vector Offset => new Vector(OffsetX, OffsetY);
+
+        static DropShadowPanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(DropShadowPanel), new FrameworkPropertyMetadata(typeof(DropShadowPanel)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            UpdateCurrentDropShadowEffect();
+            base.OnApplyTemplate();
+        }
+
+        private static void Shadow_Changed(
+            DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DropShadowPanel panel)
+            {
+                panel.UpdateCurrentDropShadowEffect();
+            }
+        }
+
+        /// <summary>
+        /// Forces an update of the <see cref="DropShadowEffect"/> property by calling
+        /// <see cref="CalculateDropShadowEffect"/> and switching the property, if
+        /// necessary.
+        /// </summary>
+        protected void UpdateCurrentDropShadowEffect()
+        {
+            DropShadowEffect newEffect = CalculateDropShadowEffect();
+            if (Effect != newEffect)
+            {
+                Effect = newEffect;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the current values for the <see cref="DropShadowEffect"/> property.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.Windows.Media.Effects.DropShadowEffect"/> which is supposed to
+        /// be applied to the panel's content.
+        /// </returns>
+        /// <remarks>
+        /// When overriding this method, try to create a cached shadow effect object whose values
+        /// are changed when this method is called again.
+        /// </remarks>
+        protected virtual DropShadowEffect CalculateDropShadowEffect()
+        {
+            if (_currentEffect == null)
+            {
+                _currentEffect = new DropShadowEffect();
+            }
+
+            SetColor();
+            SetOpacity();
+            SetBlurRadius();
+            SetDirection();
+            SetShadowDepth();
+            
+            return _currentEffect;
+        }
+
+        private void SetColor()
+        {
+            _currentEffect.Color = ShadowColor;
+        }
+
+        private void SetOpacity()
+        {
+            _currentEffect.Opacity = ShadowOpacity;
+        }
+
+        private void SetBlurRadius()
+        {
+            _currentEffect.BlurRadius = BlurRadius;
+        }
+
+        private void SetDirection()
+        {
+            double radians = Atan2(OffsetY, OffsetX);
+            double degrees = radians * (180 / PI);
+            _currentEffect.Direction = 360 - degrees;
+            Debug.WriteLine(degrees);
+        }
+
+        private void SetShadowDepth()
+        {
+            _currentEffect.ShadowDepth = Offset.Length;
+        }
+
+    }
+
+}
