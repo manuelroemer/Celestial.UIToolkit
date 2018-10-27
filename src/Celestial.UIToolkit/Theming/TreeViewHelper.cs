a9a7a2b9f672b6c3f2ad2d36b3aac078e1c2bf58
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-10-27 14:48:39 +0200

    Added Size variations.

--- src/Celestial.UIToolkit/Theming/TreeViewHelper.cs
+++ src/Celestial.UIToolkit/Theming/TreeViewHelper.cs
@@ -1,16 +1,36 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Celestial.UIToolkit.Theming
 {
 
+    /// <summary>
+    /// Provides members which are required for implementing the default style of a
+    /// <see cref="TreeViewItem"/>.
+    /// </summary>
     internal static class TreeViewHelper
     {
 
         /// <summary>
         /// An attached dependency property which is used to get and set the depth of a
         /// TreeViewItem from a style.
-        /// Setting this property works recursively.
+        /// Check the example for seeing how this can be implemented.
         /// </summary>
+        /// <example>
+        ///     &lt;Setter Property="theming:TreeViewHelper.DepthLevel"&gt;
+        ///         &lt;Setter.Value&gt;
+        ///             &lt;Binding Path="(theming:TreeViewHelper.DepthLevel)"
+        ///                      RelativeSource="{RelativeSource AncestorType=TreeViewItem}"
+        ///                      FallbackValue="0"
+        ///                      TargetNullValue="0"
+        ///                      ConverterParameter="1"&gt;
+        ///                 &lt;Binding.Converter&gt;
+        ///                     &lt;c:MathOperationConverter Operator="Add" /&gt;
+        ///                 &lt;/Binding.Converter&gt;
+        ///             &lt;/Binding&gt;
+        ///         &lt;/Setter.Value&gt;
+        ///     &lt;/Setter&gt;
+        /// </example>
         public static readonly DependencyProperty DepthLevelProperty =
             DependencyProperty.RegisterAttached(
                 "DepthLevel",
