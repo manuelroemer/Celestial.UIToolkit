83476190102c76ae3ac4b1685f2e0737e04fe897
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-10-27 14:33:21 +0200

    Implemented the basic TreeViewItem.

---
+++ src/Celestial.UIToolkit/Theming/TreeViewHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Celestial.UIToolkit.Theming
+{
+
+    internal static class TreeViewHelper
+    {
+
+        /// <summary>
+        /// An attached dependency property which is used to get and set the depth of a
+        /// TreeViewItem from a style.
+        /// Setting this property works recursively.
+        /// </summary>
+        public static readonly DependencyProperty DepthLevelProperty =
+            DependencyProperty.RegisterAttached(
+                "DepthLevel",
+                typeof(int),
+                typeof(TreeViewHelper),
+                new PropertyMetadata(0));
+        
+        public static int GetDepthLevel(DependencyObject obj) =>
+            (int)obj.GetValue(DepthLevelProperty);
+        
+        public static void SetDepthLevel(DependencyObject obj, int value) =>
+            obj.SetValue(DepthLevelProperty, value);
+        
+    }
+
+}
