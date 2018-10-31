4fcaba4904b410ef74968a0c56c115a5859dc384
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-10-31 11:34:55 +0100

    Added additional window commands. Renamed the properties.

--- src/Celestial.UIToolkit/Controls/WindowCommands.cs
+++ src/Celestial.UIToolkit/Controls/WindowCommands.cs
@@ -17,21 +17,21 @@ namespace Celestial.UIToolkit.Controls
         /// For this command to work, you need to bind the CommandParameter to a
         /// <see cref="Window"/> instance.
         /// </summary>
-        public static ICommand MinimizeWindowCommand { get; } = new MinimizeWindowCommandImpl();
+        public static ICommand MinimizeCommand { get; } = new MinimizeCommandImpl();
 
         /// <summary>
         /// Gets a command which can be used to maximize or restore a window.
         /// For this command to work, you need to bind the CommandParameter to a
         /// <see cref="Window"/> instance.
         /// </summary>
-        public static ICommand MaximizeWindowCommand { get; } = new MaximizeWindowCommandImpl();
+        public static ICommand MaximizeCommand { get; } = new MaximizeCommandImpl();
 
         /// <summary>
         /// Gets a command which can be used to close a window.
         /// For this command to work, you need to bind the CommandParameter to a
         /// <see cref="Window"/> instance.
         /// </summary>
-        public static ICommand CloseWindowCommand { get; } = new CloseWindowCommandImpl();
+        public static ICommand CloseCommand { get; } = new CloseCommandImpl();
 
         /// <summary>
         /// Gets a command which can be used to show a window's system menu.
@@ -40,16 +40,37 @@ namespace Celestial.UIToolkit.Controls
         /// </summary>
         public static ICommand ShowSystemMenuCommand { get; } = new ShowSystemShowSystemMenuCommandImpl();
 
+        /// <summary>
+        /// Gets a command which can be used to show a window.
+        /// For this command to work, you need to bind the CommandParameter to a
+        /// <see cref="Window"/> instance.
+        /// </summary>
+        public static ICommand ShowCommand { get; } = new ShowCommandImpl();
+
+        /// <summary>
+        /// Gets a command which can be used to show a window as a dialog.
+        /// For this command to work, you need to bind the CommandParameter to a
+        /// <see cref="Window"/> instance.
+        /// </summary>
+        public static ICommand ShowDialogCommand { get; } = new ShowDialogCommandImpl();
+
         private abstract class WindowCommand : ICommand
         {
 
             public event EventHandler CanExecuteChanged;
 
-            public bool CanExecute(object parameter) => parameter is Window;
+            public bool CanExecute(object parameter) => true;
 
             public void Execute(object parameter)
             {
-                ExecuteCore((Window)parameter);
+                // This check should intuitively go into CanExecute, but there are situations in
+                // which CanExecute doesn't update.
+                // Thus, the command gets flagged as not executable, even though it is.
+                // By doing the check here, we are on the save side.
+                if (parameter is Window window)
+                {
+                    ExecuteCore(window);
+                }
             }
 
             protected abstract void ExecuteCore(Window window);
@@ -61,7 +82,7 @@ namespace Celestial.UIToolkit.Controls
 
         }
 
-        private class CloseWindowCommandImpl : WindowCommand
+        private class CloseCommandImpl : WindowCommand
         {
 
             protected override void ExecuteCore(Window window)
@@ -71,9 +92,8 @@ namespace Celestial.UIToolkit.Controls
 
         }
 
-        private class MinimizeWindowCommandImpl : WindowCommand
+        private class MinimizeCommandImpl : WindowCommand
         {
-
             protected override void ExecuteCore(Window window)
             {
                 if (window.WindowState == WindowState.Minimized)
@@ -81,12 +101,10 @@ namespace Celestial.UIToolkit.Controls
                 else
                     SystemCommands.MinimizeWindow(window);
             }
-
         }
 
-        private class MaximizeWindowCommandImpl : WindowCommand
+        private class MaximizeCommandImpl : WindowCommand
         {
-
             protected override void ExecuteCore(Window window)
             {
                 if (window.WindowState == WindowState.Maximized)
@@ -94,12 +112,10 @@ namespace Celestial.UIToolkit.Controls
                 else
                     SystemCommands.MaximizeWindow(window);
             }
-
         }
 
         private class ShowSystemShowSystemMenuCommandImpl : WindowCommand
         {
-
             protected override void ExecuteCore(Window window)
             {
                 SystemCommands.ShowSystemMenu(
@@ -110,7 +126,22 @@ namespace Celestial.UIToolkit.Controls
                     )
                 );
             }
+        }
 
+        private class ShowCommandImpl : WindowCommand
+        {
+            protected override void ExecuteCore(Window window)
+            {
+                window.Show();
+            }
+        }
+
+        private class ShowDialogCommandImpl : WindowCommand
+        {
+            protected override void ExecuteCore(Window window)
+            {
+                window.ShowDialog();
+            }
         }
 
     }
