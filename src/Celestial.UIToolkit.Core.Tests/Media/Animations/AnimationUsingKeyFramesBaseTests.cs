90f28f1932d31f51738f80382379d6e5483411e8
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-11-06 13:08:34 +0100

    Created a first test for values past the final key frame.

--- src/Celestial.UIToolkit.Core.Tests/Media/Animations/AnimationUsingKeyFramesBaseTests.cs
+++ src/Celestial.UIToolkit.Core.Tests/Media/Animations/AnimationUsingKeyFramesBaseTests.cs
@@ -8,6 +8,12 @@ using DiscreteDoubleKeyFrame = Celestial.UIToolkit.Tests.Media.Animations.Mocks.
 using LinearDoubleKeyFrame = Celestial.UIToolkit.Tests.Media.Animations.Mocks.LinearDoubleKeyFrame;
 using EasingDoubleKeyFrame = Celestial.UIToolkit.Tests.Media.Animations.Mocks.EasingDoubleKeyFrame;
 using SplineDoubleKeyFrame = Celestial.UIToolkit.Tests.Media.Animations.Mocks.SplineDoubleKeyFrame;
+using DoubleKeyFrameCollection = Celestial.UIToolkit.Tests.Media.Animations.Mocks.DoubleKeyFrameCollection;
+using Celestial.UIToolkit.Media.Animations;
+using Celestial.UIToolkit.Tests.Media.Animations.Mocks;
+using Xunit;
+using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace Celestial.UIToolkit.Tests.Media.Animations
 {
@@ -15,8 +21,23 @@ namespace Celestial.UIToolkit.Tests.Media.Animations
     public class AnimationUsingKeyFramesBaseTests
     {
 
+        [Fact]
+        public void ReturnsLastKeyFrameValueIfPastFinalKeyTime()
+        {
+            const double finalValue = 10;
 
+            var animation = new DoubleAnimationUsingKeyFrames(new Duration(TimeSpan.FromSeconds(10)));
+            var clock = ControllableAnimationClock.FromTime(TimeSpan.FromSeconds(10));
+            animation.KeyFrames = new DoubleKeyFrameCollection()
+            {
+                new DiscreteDoubleKeyFrame(finalValue, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(5)))
+            };
 
+            double result = animation.GetCurrentValue(0d, 0d, clock);
+
+            Assert.Equal(finalValue, result);
+        }
+        
     }
 
 }
