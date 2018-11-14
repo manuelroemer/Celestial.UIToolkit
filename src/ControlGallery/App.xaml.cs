9c0d4b86e5e5578e1eb0e1eec4bb0420909828f4
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-11-14 22:46:32 +0100

    Made the ControlGallery's XAML Display support dark mode.

--- src/ControlGallery/App.xaml.cs
+++ src/ControlGallery/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using Celestial.UIToolkit.Xaml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using ShowMeTheXAML;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
+using System.Xml;
 
 namespace ControlGallery
 {
@@ -8,14 +13,42 @@ namespace ControlGallery
     public partial class App : Application
     {
         
+        public App()
+        {
+            // Important: Load this here, because otherwise, the XAML won't be able to see them.
+            LoadAvalonEditEmbeddedXshdDefinitions();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            ThemeManager.Current.ChangeTheme("Light");
-
             XamlDisplay.Init();
+            ThemeManager.Current.ChangeTheme("Light");
             new GalleryBootstrapper().Run();
+        }
+
+        private static void LoadAvalonEditEmbeddedXshdDefinitions()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var xshdFiles = assembly.GetManifestResourceNames().Where(file => file.EndsWith(".xshd"));
+
+            foreach (var xshdFile in xshdFiles)
+            {
+                using (var stream = assembly.GetManifestResourceStream(xshdFile))
+                {
+                    if (stream == null)
+                        continue;
+
+                    using (var reader = new XmlTextReader(stream))
+                    {
+                        var xshdDefinition = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                        HighlightingManager.Instance.RegisterHighlighting(
+                            xshdDefinition.Name, new string[] { }, xshdDefinition
+                        );
+                    }
+                }
+            }
         }
 
     }
