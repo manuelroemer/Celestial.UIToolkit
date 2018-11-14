391df7afa5574782b33903e4022e888c4c8fdf7a
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-11-14 20:35:45 +0100

    Commented the ThemeResourceDictionary. Added toolkit/UI specific extensions to the ThemeManager in the Toolkit project. Modified the colors of the Card, depending on the current theme. Modified the Dark Theme's brushes.

--- src/Celestial.UIToolkit.Core/Xaml/ThemeResourceDictionary.cs
+++ src/Celestial.UIToolkit.Core/Xaml/ThemeResourceDictionary.cs
@@ -1,22 +1,38 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using static Celestial.UIToolkit.TraceSources;
 
 namespace Celestial.UIToolkit.Xaml
 {
 
+    /// <summary>
+    ///     A resource dictionary which provides built-in support for dynamic theme changes
+    ///     which come from the <see cref="ThemeManager"/>.
+    ///     Use the <see cref="ThemeDictionaries"/> property to specify nested, theme specific
+    ///     dictionaries.
+    /// </summary>
+    /// <remarks>
+    ///     This resource dictionary is directly modeled after the UWP ResourceDictionary class,
+    ///     which provides a similar API.
+    ///     See https://docs.microsoft.com/en-us/uwp/api/Windows.UI.Xaml.ResourceDictionary#Windows_UI_Xaml_ResourceDictionary_ThemeDictionaries
+    ///     for details.
+    /// </remarks>
     public class ThemeResourceDictionary : SharedResourceDictionary
     {
 
         private ResourceDictionary _activeThemeDictionary;
         
+        /// <summary>
+        ///     Gets a collection of <see cref="ResourceDictionary"/> instances which dynamically
+        ///     get loaded whenever the application's theme changes.
+        ///     The key under which the dictionary is stored must match the theme's name to get
+        ///     the dictionary loaded.
+        /// </summary>
         public Dictionary<object, ResourceDictionary> ThemeDictionaries { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemeResourceDictionary"/>.
+        /// </summary>
         public ThemeResourceDictionary()
         {
             ThemeDictionaries = new Dictionary<object, ResourceDictionary>();
@@ -55,13 +71,17 @@ namespace Celestial.UIToolkit.Xaml
                 return false;
             }
         }
-
+        
         private void RemoveActiveThemeDictionary()
         {
             if (_activeThemeDictionary != null)
             {
-                ResourcesSource.Verbose("Removing previous active theme dictionary..");
+                ResourcesSource.Verbose("Removing previously active theme dictionary.");
                 Application.Current.Resources.MergedDictionaries.Remove(_activeThemeDictionary);
+            }
+            else
+            {
+                ResourcesSource.Verbose("Didn't find an active theme dictionary. Nothing to remove.");
             }
         }
 
@@ -71,8 +91,15 @@ namespace Celestial.UIToolkit.Xaml
             {
                 ResourcesSource.Verbose("Adding active theme dictionary.");
                 _activeThemeDictionary = dictionary;
-                
+
+                // I would love to not add this to the global app's resources, but
+                // it breaks the resource lookup otherwise.
+                // So far, this seems to be the only working solution.
                 Application.Current.Resources.MergedDictionaries.Add(_activeThemeDictionary);
+            }
+            else
+            {
+                ResourcesSource.Verbose("Didn't find an active dictionary to load.");
             }
         }
         
