8173d45eacfe823a363b1a3c8513574392d350d1
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-11-15 15:09:40 +0100

    Added a Behavior class.

---
+++ src/Celestial.UIToolkit.Core/Interactivity/Behavior.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+
+namespace Celestial.UIToolkit.Interactivity
+{
+
+    /// <summary>
+    /// An object which can be attached to a <see cref="DependencyObject"/> and thus provide
+    /// external behaviors for the associated object.
+    /// </summary>
+    public class Behavior : DependencyObject, IBehavior
+    {
+
+        /// <summary>
+        /// Gets the object to which this behavior is attached.
+        /// </summary>
+        public DependencyObject AssociatedObject { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this behavior is currently attached to an object.
+        /// </summary>
+        protected internal bool IsAttached => AssociatedObject != null;
+
+        /// <summary>
+        /// Attaches this behavior to the specified <paramref name="associatedObject"/>.
+        /// </summary>
+        /// <param name="associatedObject">
+        /// A <see cref="DependencyObject"/> to which this behavior will be attached.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="associatedObject"/> is null.
+        /// </exception>
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is null)
+                throw new ArgumentNullException(nameof(associatedObject)); 
+    
+            if (IsAttached)
+            {
+                if (associatedObject == AssociatedObject)
+                {
+                    return;
+                }
+                else
+                {
+                    throw new InvalidOperationException(
+                        "A Behavior can only be attached to a single object at a time."
+                    );
+                }
+            }
+
+            AssociatedObject = associatedObject;
+            OnAttached();
+        }
+
+        /// <summary>
+        /// Detaches the behavior from the current associated object.
+        /// </summary>
+        public void Detach()
+        {
+            if (!IsAttached)
+                return;
+            OnDetaching();
+            AssociatedObject = null;
+        }
+
+        /// <summary>
+        /// Called when the behavior was attached to an associated object.
+        /// Can be overridden to initialize functionality regarding the associated object
+        /// (e.g. setting up event handlers).
+        /// </summary>
+        protected virtual void OnAttached()
+        {
+        }
+
+        /// <summary>
+        /// Called when the behavior is about to be detached from the current 
+        /// <see cref="AssociatedObject"/>.
+        /// At this point in time, the <see cref="AssociatedObject"/> still holds a value.
+        /// Can be overridden to un-initialize functionality which was previously setup in the
+        /// <see cref="OnAttached"/> method (e.g. removing event handlers).
+        /// </summary>
+        protected virtual void OnDetaching()
+        {
+        }
+
+    }
+}
---
+++ src/Celestial.UIToolkit.Core/Interactivity/IBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Celestial.UIToolkit.Interactivity
+{
+
+    /// <summary>
+    /// Represents an object which can be attached to a <see cref="DependencyObject"/>
+    /// and thus provide external behaviors for the associated object.
+    /// </summary>
+    public interface IBehavior
+    {
+
+        /// <summary>
+        /// Gets the object to which this behavior is attached.
+        /// </summary>
+        DependencyObject AssociatedObject { get; }
+
+        /// <summary>
+        /// Attaches this behavior to the specified <paramref name="associatedObject"/>.
+        /// </summary>
+        /// <param name="associatedObject">
+        /// A <see cref="DependencyObject"/> to which this behavior will be attached.
+        /// </param>
+        void Attach(DependencyObject associatedObject);
+
+        /// <summary>
+        /// Detaches this behavior from the current associated object.
+        /// </summary>
+        void Detach();
+
+    }
+
+}
